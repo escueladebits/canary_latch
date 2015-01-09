23fd851e5b65418d1b2603db97f5f2a0b2e8e7d5
Antonio J. S. Padial <antonio.spadial@cchs.csic.es>
2015-01-10 00:51:36 +0100

    Added scaffolding for Plugin Class.

---
+++ src/com/escueladebits/canary_latch/LatchPlugin.java
@@ -0,0 +1,65 @@
+/*
+Copyright © 2015 Antonio Jesús Sánchez Padial
+
+This file is part of Canary Latch.
+
+Canary Latch is free software; you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 2.1 of the License, or
+(at your option) any later version.
+
+Canary Latch is distributed in the hope that it will be useful
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with Canary Latch. If not see <http://www.gnu.org/licenses/>.
+*/
+
+package com.escueladebits.canary_latch;
+
+import net.canarymod.plugin.Plugin;
+
+/**
+ * This class provides an additional security access layer to a CanaryMod
+ * server based in <a href="latch.elevenpaths.com">Latch</a>.
+ * <p>
+ * The class register listeners for reacting to user accesses or changes in the 
+ * latch account. It also provides CLI commands to pair minecraft accounts with
+ * its latch correspondant, to unpair it, and to update the status (banned or
+ * not) of current users. 
+ */
+public class LatchPlugin extends Plugin {
+    /**
+     * Starts the plugin. Registers listeners for user connections and latch
+     * updates, and register commands for pairing, unpairing and updating 
+     * current users.
+     *
+     * @return	boolean
+     */
+    @Override
+    public boolean enable() {
+        // Read latch configuration from canary setup
+
+        // Create latch manager
+
+        // Register connection listener
+
+        // Register pairing command
+
+        // Register unpairing command
+
+        // Register updating command
+
+        // Update current user
+        return false;
+    }
+
+    /**
+     * Stops the plugin.
+     */
+    @Override
+    public void disable() {
+    }
+ }
