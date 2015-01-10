2e7f2c474005b06dbbe90bd4b4e77677edafda2a
Antonio J. S. Padial <antonio.spadial@cchs.csic.es>
2015-01-10 11:48:02 +0100

    Added latch integration to LatchManager.

--- src/com/escueladebits/canary_latch/LatchManager.java
+++ src/com/escueladebits/canary_latch/LatchManager.java
@@ -22,14 +22,16 @@ package com.escueladebits.canary_latch;
 import com.elevenpaths.latch.Latch;
 import com.elevenpaths.latch.LatchResponse;
 import com.elevenpaths.latch.Error;
+import com.google.gson.JsonObject;
+import net.canarymod.api.entity.living.humanoid.Player;
 
 /**
  * This class of 
  */
-public class LatchManager implements PluginListener {
+public class LatchManager {
 
-    private String secret;
-    private String key;
+    private String secretKey;
+    private String applicationId;
 
     private Latch latch;
 
@@ -39,50 +41,75 @@ public class LatchManager implements PluginListener {
      * @param secret     The secret
      * @param key        The key
      */
-    public LatchManager(String secret, String key) {
-        this.secret = secret;
-        this.key = key;
-        latch = new Latch(this.secret, this.key);
+    public LatchManager(String applicationId, String secretKey) {
+        this.secretKey = secretKey;
+        this.applicationId = applicationId;
+        latch = new Latch(this.applicationId, this.secretKey);
     }
 
     /**
+     *
      * @param player     A Minecraft account
+     * @param token      A latch token the user received in her phone
      */
-    public void pairPlayer(Player player) {
-        // TODO
+    public void pairPlayer(Player player, String token) {
+        LatchResponse response = latch.pair(token);
+        if (response.getError() == null) {
+            JsonObject data = response.getData();
+            String latchAccount = data.get("accountId").getAsString();
+            setLatchAccount(player, latchAccount);
+        }
+        else {
+            // TODO: manage error
+        }
     }
 
     /**
+     *
      * @param player    A Minecraft account
      */
     public void unpairPlayer(Player player) {
-        // TODO
+        String latchAccount = getLatchAccount(player);
+        latch.unpair(latchAccount);
     }
 
     /**
-     * Retrieves player latch status from latch service.ç
+     * Retrieves player latch status from latch service.
      * 
      * @param player     A Minecraft player.
      */
     public void updateStatus(Player player) {
-        /*
-        if !player.isBanned():
-            latchAccount = getLatchAccount(player)
-            boolean status = latch.retrieveStatus(latchAccount)
-            if !status:
-                latchBan(player)
-        */
+        String latchAccount = getLatchAccount(player);
+        if (latchAccount == "") {
+            LatchResponse response = latch.status(latchAccount);
+            if (response.getError() == null) {
+                JsonObject data = response.getData();
+                JsonObject operations = data.getAsJsonObject("operations");
+                JsonObject application = operations.getAsJsonObject(applicationId);
+                String status = application.get("status").getAsString();
+                setLatchStatus(player, status);
+            }
+            else {
+                // TODO: manage Error
+            }
+        }
+        else {
+            // Clean status
+            setLatchStatus(player, "");
+        }
     }
 
     /**
      *
+     * @param player     A Minecraft player
      */
     public boolean isLatchOut(Player player) {
-        return true;
+        return getLatchStatus(player) == "off";
     }
 
     /**
      *
+     * @param player     A Minecraft player
      */
     public void latchBan(Player player) {
         /*
@@ -93,13 +120,23 @@ public class LatchManager implements PluginListener {
 
     private String getLatchAccount(Player player) {
         // return CanaryDB.getField(player, 'latch_account')
+        return "";
     }
 
     private void setLatchAccount(Player player, String latchAccount) {
-        // CanaryDb.setField(player, 'latch_account', latchAccount)
+        // CanaryDB.setField(player, 'latch_account', latchAccount)
+    }
+
+    private String getLatchStatus(Player player) {
+        // return CanaryDB.getField(player, 'latch_status')
+        return "off";
+    }
+
+    private void setLatchStatus(Player player, String status) {
+        // CanaryDB.setField(player, 'latch_status', status);
     }
 
     private void removeLatchAccount(Player player) {
-        setLatchAccount(player, '');
+        setLatchAccount(player, "");
     }
 }
